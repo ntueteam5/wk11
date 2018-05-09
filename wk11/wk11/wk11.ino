8d14ccb4870ae871217a779c668cc8885d7682bd
ntueteam5 <39110121+ntueteam5@users.noreply.github.com>
2018-05-09 11:38:58 +0800

    Add files via upload

---
+++ wk11/wk11/wk11.ino
@@ -0,0 +1,19 @@
+int LED_PIN1=13;
+int LED_PIN2=12;
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_PIN1, OUTPUT);
+  pinMode(LED_PIN2, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(LED_PIN1, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(300);                       // wait for a second
+  digitalWrite(LED_PIN1, LOW);    // turn the LED off by making the voltage LOW
+  delay(300);                       // wait for a second
+  digitalWrite(LED_PIN2, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(300);                       // wait for a second
+  digitalWrite(LED_PIN2, LOW);    // turn the LED off by making the voltage LOW
+  delay(300);                       // wait for a second
+}
